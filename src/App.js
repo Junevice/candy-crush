7f91321f66734db5109d66f08fc28fed96c251a3
Junevice <june.webfreelance@gmail.com>
2022-05-07 17:43:12 +0200

    19-35 - Logic of generated array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useState, useEffect} from "react";
 
-function App() {
+const width=8;
+const candyColors=['blue', 'green', 'organge', 'yellow', 'purple', 'red'];
+
+
+const App = () => {
+
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+  const createBoard = () => {
+    const randomColorArrangement=[];
+    for(let i=0; i<width*width; i++){
+      const randomColor=candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrangement(randomColorArrangement);
+  }
+
+
+  useEffect(()=>{
+    createBoard()
+  }, []);
+  
+  console.log(currentColorArrangement)
+
   return (
     <div className="App">
-      
+      <h1>Hey</h1>
     </div>
   );
 }
